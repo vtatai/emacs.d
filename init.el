63bde03d838cb6186538f99dfb3e69243e3f8914
Victor Tatai <vtatai@fitbit.com>
2016-10-04 16:30:40 -0700

    Initial commit

---
+++ init.el
@@ -0,0 +1,23 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(tool-bar-mode -1)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (## neotree))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/") 
+
+(global-set-key [f8] 'neotree-toggle)
